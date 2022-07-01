2628d06cf82406f848b1a8602f340f7f17e609a6
Jonmuxu <Jonmuxupvp@gmail.com>
2022-07-01 14:44:24 +0200

    Created Dream

---
+++ src/dev/jonmuxu/Dream.java
@@ -0,0 +1,34 @@
+package dev.jonmuxu;
+
+import java.time.LocalDate;
+
+public class Dream {
+	private String title;
+	private String content;
+	private LocalDate date;
+	private boolean isLucid;
+
+	public Dream(String title, String content, LocalDate date) {
+		this.title = title;
+		this.content = content;
+		this.date = date;
+		this.isLucid = this.title.toLowerCase().contains("Lucid");
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public String getContent() {
+		return this.content;
+	}
+
+	public String getDate() {
+		return this.date.toString();
+	}
+
+	@Override
+	public String toString() {
+		return this.content;
+	}
+}
